845aab59ed4a7bcd3181b08c6d9b935427f4eebe
Oskari Okko Ojala <okko@frantic.com>
2016-10-16 22:03:51 +0300

    Remove WP installers with profile.d

---
+++ .profile.d/01-remove-installers.sh
@@ -0,0 +1,5 @@
+rm -rf $HOME/web/wp/wp-admin/install.php
+rm -rf $HOME/web/wp/wp-admin/install-helper.php
+rm -rf $HOME/web/wp/wp-admin/setup-config.php
+rm -rf $HOME/web/wp/wp-admin/update.php
+rm -rf $HOME/web/wp/wp-admin/update-core.php
