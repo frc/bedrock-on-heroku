7d8dc234c96cfa209745bd605f04e4d330b22a1b
Janne Aalto <janne.aalto@frantic.com>
2017-02-22 12:42:10 +0700

    memory limit, installers, config

--- .profile.d/001-remove-installers.sh
+++ .profile.d/001-remove-installers.sh
@@ -1,5 +1,5 @@
-rm -rf $HOME/web/wp/wp-admin/install.php
-rm -rf $HOME/web/wp/wp-admin/install-helper.php
-rm -rf $HOME/web/wp/wp-admin/setup-config.php
-rm -rf $HOME/web/wp/wp-admin/update.php
-rm -rf $HOME/web/wp/wp-admin/update-core.php
+rm -rf $HEROKU_APP_DIR/wp/wp-admin/install.php
+rm -rf $HEROKU_APP_DIR/wp/wp-admin/install-helper.php
+rm -rf $HEROKU_APP_DIR/wp/wp-admin/setup-config.php
+rm -rf $HEROKU_APP_DIR/wp/wp-admin/update.php
+rm -rf $HEROKU_APP_DIR/wp/wp-admin/update-core.php
